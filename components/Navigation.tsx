d82321f41350179aaca246f63cb29d868903692d
webriq <info@webriq.com>
2025-02-28 17:15:27 +0800

    Test

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -5,11 +5,39 @@ import { navigations } from "../lib/navigation";
 
 export default function Navigation() {
   return (
-    <nav className={`sticky`}>
+    <nav
+      className={``}
+      style={{
+        backgroundColor: `black`,
+        width: "100%",
+        position: "fixed",
+        display: "top-0",
+      }}
+    >
       {navigations && navigations.length > 1 ? (
-        <div className="w-full flex max-w-[1280px] mx-auto">
+        <div
+          className="w-full flex max-w-[1280px] mx-auto"
+          style={{
+            maxWidth: "1280px",
+            margin: "0 auto",
+            height: "40px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           {navigations?.map((nav, idx) => (
-            <Link key={idx} href={nav?.link} className="uppercase">
+            <Link
+              key={idx}
+              href={nav?.link}
+              className=""
+              style={{
+                listStyle: "none",
+                padding: "0 20px",
+                textDecoration: "none",
+                textTransform: "uppercase",
+              }}
+            >
               {nav?.title}
             </Link>
           ))}
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,3 @@
-// /** @type {import('postcss').Config} */
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// };
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,

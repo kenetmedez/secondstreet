451099568f95f2d0762f9c7a3a3e6147034cd4d4
kenetmedez <kenetmedez@gmail.com>
2025-03-15 16:19:09 +0800

    Fix: Navigation => Added sidebar

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,11 +4,17 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 import { IoReorderThreeOutline } from "react-icons/io5";
+import { GrClose } from "react-icons/gr";
 
 import { navigations } from "../lib/navigation";
 
 export default function Navigation() {
   const [scrolled, setScrolled] = useState(false);
+  const [sideMenu, setSideMenu] = useState(false);
+
+  const handleSideMenu = () => {
+    setSideMenu((prev) => !prev);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -23,14 +29,16 @@ export default function Navigation() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
     <nav
-      className={`fixed ${scrolled ? "bg-black/80 py-8" : "bg-transparent py-8"} top-0 z-[999] w-full !mx-auto h-[40px] flex items-center justify-center duration-300 transition-all ease-in-out`}
+      className={`fixed ${scrolled ? "bg-black/80 py-8" : "bg-transparent py-8"} top-0 z-[999] w-full !mx-auto h-[40px] flex items-center justify-center duration-300 transition-all ease-in-out
+      `}
     >
       {navigations && navigations.length > 1 ? (
         <div className="w-full flex max-w-[1280px] mx-auto flex-row items-center justify-center">
           <div className="md:w-1/4 w-full flex items-center justify-center px-2">
-            <span className="lg:text-3xl text-xl text-yellow-500 text-center">
+            <span className="lg:text-3xl text-xl text-yellow-500 text-center font-lobster">
               second street
             </span>
             {/* <Image
@@ -47,17 +55,59 @@ export default function Navigation() {
               <Link
                 key={idx}
                 href={nav?.link}
-                className="uppercase text-white px-10"
+                className="text-white px-10 font-spicy"
               >
                 {nav?.title}
               </Link>
             ))}
           </div>
 
-          <div className="mobile-nav lg:w-3/4 w-full lg:hidden flex items-center justify-end px-4">
+          <div className="mobile-nav relative lg:w-3/4 w-full lg:hidden flex items-center justify-end px-4">
             <span className="text-white">
-              <IoReorderThreeOutline size={25} />
+              <IoReorderThreeOutline
+                size={25}
+                onClick={handleSideMenu}
+                className="text-white"
+              />
             </span>
+          </div>
+
+          <div
+            className={`absolute md:flex lg:hidden flex-col top-0 -right-96 bg-black h-screen min-w-[300px] p-4 z-50 gap-4 transition-all duration-300 ease-in-out ${sideMenu ? "scale-100 right-0" : "scale-0"}`}
+          >
+            <div className="p-2">
+              <GrClose
+                onClick={handleSideMenu}
+                className="relative text-white hover:rotate-180 cursor-pointer"
+              />
+            </div>
+
+            <div className="w-full flex items-center">
+              <Link
+                href="/"
+                className="w-full font-lobster text-yellow-500 text-center text-xl"
+                onClick={handleSideMenu}
+              >
+                second street
+              </Link>
+            </div>
+
+            <div className="w-full">
+              <ul className="gap-2 flex flex-col">
+                {navigations?.map((nav, idx) => (
+                  <li key={idx}>
+                    <Link
+                      href={nav?.link}
+                      key={idx}
+                      className="text-white px-2 font-spicy hover:underline transform-all duration-300 ease-linear"
+                      onClick={handleSideMenu}
+                    >
+                      {nav?.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       ) : null}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Lobster, Spicy_Rice } from "next/font/google";
 import "../../styles/globals.css";
 
 const geistSans = Geist({
@@ -7,9 +7,16 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const lobster = Lobster({
+  variable: "--font-lobster",
   subsets: ["latin"],
+  weight: "400",
+});
+
+const spicy = Spicy_Rice({
+  variable: "--font-spicy",
+  subsets: ["latin"],
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -29,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${lobster.variable} ${spicy.variable} antialiased`}
       >
         {children}
       </body>
